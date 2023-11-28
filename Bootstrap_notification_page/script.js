56f085f49e94bd11cc6c5b67b7588a1515c89271
giorgio <giorgio.programmer@gmail.com>
2023-11-28 11:56:32 +0100

    Update script.js

--- Bootstrap_notification_page/script.js
+++ Bootstrap_notification_page/script.js
@@ -16,3 +16,18 @@ $(document).ready(function(){
             }, 3000);
         });
     });
+let fullscreen;
+let fsEnter = document.getElementById('fullscr');
+fsEnter.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = "Exit Fullscreen";
+    }
+    else {
+        fullscreen = false;
+        document.exitFullscreen();
+        fsEnter.innerHTML = "Go Fullscreen";
+    }
+});
